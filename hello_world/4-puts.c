f822c3b9eb966a6c2476ddc6b3b8848320ec054e
Hikarikedo <keriandouidi01@gmail.com>
2024-10-21 15:54:37 +0200

    my first c program

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,14 @@
-#include<stdio.h>
+#include "stdio.h"
+
 /**
- *main - Entry point
- *Return: Always 0 (Success)
+ * main - Entry point
+ * Return: Always 0 (Sucess)
  */
+
 int main(void)
 
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (O);
+	return (0);
 
 }
