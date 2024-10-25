fae1c7b9d45d2a370ab073c29e900a742d5394ed
Hikarikedo <keriandouidi01@gmail.com>
2024-10-25 18:37:41 +0200

    times table

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * add - Adds two integers.
+ * @num1: The first integer to be added.
+ * @num2: The second integer to be added.
+ *
+ * Return: The result of the addition.
+ */
+int add(int b, int a)
+
--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,35 +1,35 @@
 #include "main.h"
+
 /**
- * times_table - prints time tables from 0 to 9
+ * times_table - Prints the 9 times table, starting with 0.
  */
 void times_table(void)
 {
-	int i = 0;
-	int j = 0;
-	int res = 0;
+	int num, mult, prod;
 
-	for (i = 0; i <= 9; i++)
-{
-	for (j = 0; j <= 9; j++)
-{
-	res = i * j;
-	if (res < 10)
-{
-	if (j != 0)
-	_putchar(' ');
-	_putchar(res + '0');
-}
-	else
-{
-	_putchar(res / 10 + '0');
-	_putchar(res % 10 + '0');
-}
-	if (j < 9)
-{
-	_putchar(',');
-	_putchar(' ');
-}
-}
-	_putchar('\n');
-}
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+			_putchar((prod / 10) + '0');
+			}
+
+			_putchar((prod % 10) + '0');
+
+		}
+		_putchar('\n');
+	}
 }
