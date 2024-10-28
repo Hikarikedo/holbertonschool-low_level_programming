b6b445e1b10101693af2e91e0b5b5620dc64dfac
Hikarikedo <keriandouidi01@gmail.com>
2024-10-28 11:52:09 +0100

    uppercase checker

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_isupper - chekcs if characters are uppercase
+ *
+ *c - characters that has to be checked
+ *
+ * @c parameter: checking c characters
+ *
+ * Return: 1 if char is uppercase, otherwise 0 if lowercase
+ */
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
