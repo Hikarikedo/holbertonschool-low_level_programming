428f711352d2c37eb9ab225b98809711396a0d0f
Hikarikedo <keriandouidi01@gmail.com>
2024-10-29 15:12:13 +0100

    printing triangles

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+void print_triangle(int size)
+{
+	int a;
+	int b;
+
+	if (size <= 0)
+{
+	_putchar('\n');
+}
+	for (a = 1; a <= size; a++)
+{
+	for (b = size - a; b > 0; b--)
+{
+	_putchar(' ');
+}
+	for (b = 1; b <= a; b++)
+{
+	_putchar('#');
+}
+	_putchar('\n');
+}
+}
