0ac72e0b3feb58cbf222606039c445f9356a6fcd
Hikarikedo <keriandouidi01@gmail.com>
2024-10-29 20:03:25 +0100

    swapping values

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ *reset_to_98 - updates the value of the n variable to 98
+ *@n: variable with original value of 402
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *swap_int - swaps values of a and b variables
+ *@a: variable with a value of 98 before swap
+ *@b: variable with a value of 42 before swap
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
