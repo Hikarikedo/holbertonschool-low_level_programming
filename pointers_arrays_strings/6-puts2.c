e0998f578e3ba9ccf27e3d417bd16c6fc5ab1776
Hikarikedo <keriandouidi01@gmail.com>
2024-10-30 17:40:28 +0100

    printing every 2 characters

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,11 +6,13 @@
 void puts2(char *str)
 {
 	int index = 0;
+	int a = 0;
 
 	while (str[index] != '\0')
 {
-	_putchar(str[index]);
+	_putchar(str[a]);
 	index += 2;
+	a += 2;
 }
 	_putchar('\n');
 }
