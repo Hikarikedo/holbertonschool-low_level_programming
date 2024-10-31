1a05642c4b290cc0ced60b43e72c58bf11d7b490
Hikarikedo <keriandouidi01@gmail.com>
2024-10-31 12:12:52 +0100

    skipping 1 everytime

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,13 +6,15 @@
 void puts2(char *str)
 {
 	int index = 0;
-	int a = 0;
+	int a;
 
 	while (str[index] != '\0')
 {
+	index++;
+}
+	for (a = 0; a < index; a += 2)
+{
 	_putchar(str[a]);
-	index += 2;
-	a += 2;
 }
 	_putchar('\n');
 }
