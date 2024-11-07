9be80028236b46d2f840f76ca33b6069495750e0
Hikarikedo <keriandouidi01@gmail.com>
2024-11-07 18:01:30 +0100

    args

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+/**
+ *main - print number of arguments
+ *@argc: argument count
+ *@argv: array of arguments
+ *Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
