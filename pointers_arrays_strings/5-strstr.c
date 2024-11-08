8fc1af84b4850858fc43bf5d304c1072dfdf1a56
Hikarikedo <keriandouidi01@gmail.com>
2024-11-08 14:48:41 +0100

    strstr

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -7,24 +7,24 @@
  */
 char _strstr(char *haystack, char *needle)
 {
-    if (*needle == '\0')
-        return (haystack);
+	if (*needle == '\0')
+		return (haystack);
 
-    while (*haystack)
-    {
-        char *h = haystack;
-        char *n = needle;
+	while (*haystack)
+	{
+		char *h = haystack;
+		char *n = needle;
 
-        while (*h && *n && (*h == *n))
-        {
-            h++;
-            n++;
-        }
-        if (*n == '\0')
+		while (*h && *n && (*h == *n))
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
 
-        return (haystack);
+			return (haystack);
 
-        haystack++;
-    }
-    return ('\0');
+		haystack++;
+	}
+	return ('\0');
 }
