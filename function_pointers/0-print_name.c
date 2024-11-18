21626387b96dccfe7291abeff0e63eb486d235f1
Hikarikedo <keriandouidi01@gmail.com>
2024-11-18 18:00:53 +0100

    added comments for betty

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 /**
  *print_name - prints a name
- *name: a name
- *
+ *@name: a name
+ *@f: function pointer
  *return: void = nothing
  */
 void print_name(char *name, void (*f)(char *))
