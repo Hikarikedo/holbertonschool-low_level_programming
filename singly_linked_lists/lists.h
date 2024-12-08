144409de22c1473c0ec2956f142ee7bd0d072501
Hikarikedo <keriandouidi01@gmail.com>
2024-12-08 12:43:57 +0100

    added libraries in lists.h

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,11 +1,17 @@
-#ifndef main_h
-#define main_h
+#ifndef LISTS_H
+#define LISTS_H
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 /**
  * 
  * struct list_s - singly linked list
